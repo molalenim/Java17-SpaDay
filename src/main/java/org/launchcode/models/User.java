4572a9c826ba9b96c364386137978997ee11ecf7
molalenim <molaleni.mabaso@gmail.com>
2024-05-18 21:54:29 -0400

    c) Class 11 Studio: Spa Day! pt two

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,16 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @Size(min = 5, max = 15, message = "Username must be between 5-15 characters")
+    @NotBlank(message = "Must enter a username")
     private String username;
+    @Email(message = "Must be valid email address")
     private String email;
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
     public User() {
